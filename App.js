d38e8e7ca426f44f12e38fbb0b730e7933c78148
akshita-2505 <akshitapatel522@gmail.com>
2019-04-22 14:18:55 +0530

    instagram and facebook 2 factor authentication

--- App.js
+++ App.js
@@ -7,9 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 var { FBLogin, FBLoginManager } = require('react-native-facebook-login');
+import InstagramLogin from 'react-native-instagram-login'
+import RNAccountKit, {
+  Color,
+  StatusBarStyle,
+} from 'react-native-facebook-account-kit'
 
 
 const instructions = Platform.select({
@@ -18,6 +23,7 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -30,6 +36,9 @@ export default class App extends Component<Props> {
       accountName: '', // [Android] specifies an account name on the device that should be used
       iosClientId: '756143807640-k5cs04jjch0h1r6f5bdtlm5kfdm5tnki.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     });
+
+
+
 
   }
 
@@ -53,6 +62,37 @@ export default class App extends Component<Props> {
     }
   };
 
+  facebookAuthentication = async  () => {
+    // Configures the SDK with some options
+    // RNAccountKit.configure({
+    //   responseType: 'token'|'code',
+    //   titleType: 'login',
+    //   initialAuthState: '',
+    //   initialEmail: 'some.initial@email.com',
+    //   initialPhoneCountryPrefix: '+91', // autodetected if none is provided
+    //   initialPhoneNumber: '123-456-7890',
+    //   facebookNotificationsEnabled: true|false, // true by default
+    //   readPhoneStateEnabled: true|false, // true by default,
+    //   receiveSMS: true|false, // true by default,
+    //   countryWhitelist: ['AR'], // [] by default
+    //   countryBlacklist: ['US'], // [] by default
+    //   defaultCountry: 'AR',
+    //   viewControllerMode: 'show'|'present', // for iOS only, 'present' by default
+    //   getACallEnabled: true|false
+    // })
+
+
+    RNAccountKit.loginWithPhone()
+        .then((token) => {
+          if (!token) {
+            alert('Login cancelled')
+          } else {
+            debugger
+            alert(`Logged with phone. Token: ${token}`)
+          }
+        }).catch((e)=>alert(e))
+  }
+
   render() {
 
     /*
@@ -64,9 +104,7 @@ export default class App extends Component<Props> {
             disabled={false} />
             */
 
-    return (
-      <View style={styles.container}>
-
+    /*
         <FBLogin style={{ marginBottom: 10, }}
                  ref={(fbLogin) => { this.fbLogin = fbLogin }}
                  permissions={["email","user_friends"]}
@@ -77,6 +115,29 @@ export default class App extends Component<Props> {
                    alert(data.credentials )
                  }}
         />
+*/
+
+    /*
+    <TouchableOpacity onPress={()=> this.instagramLogin.show()}>
+          <Text style={{color: 'red'}}>Login</Text>
+        </TouchableOpacity>
+        <InstagramLogin
+            ref= {ref => this.instagramLogin= ref}
+            clientId='0d497564b3fb48adb01bec1ad843544d'
+            redirectUrl='http://www.inst.com'
+            scopes={['public_content', 'follower_list']}
+            onLoginSuccess={(token) => this.setState({ token })}
+            onLoginFailure={(data) => console.log(data)}
+        />
+        */
+
+    return (
+      <View style={styles.container}>
+
+        <TouchableOpacity onPress={()=> this.facebookAuthentication()}>
+          <Text style={{color: 'red'}}>Login</Text>
+        </TouchableOpacity>
+
 
         {/*<Text style={styles.welcome}>Welcome to React Native!</Text>*/}
         {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
--- android/app/src/main/java/com/abc/MainApplication.java
+++ android/app/src/main/java/com/abc/MainApplication.java
@@ -3,6 +3,8 @@ package com.abc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAccountKitPackage(),
+            new RNCWebViewPackage(),
             new FacebookLoginPackage(),
             new RNGoogleSigninPackage()
       );
